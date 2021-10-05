7074a7e8ea98e530af04e1ab26bf855650835225
Geovane-DEV20 <geovaneDEV1998@gmail.comm>
2021-10-05 01:24:56 -0300

    Trabalhando com DataAnnotations

--- MinhaDemoMVC/Models/Filme.cs
+++ MinhaDemoMVC/Models/Filme.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +10,28 @@ namespace MinhaDemoMVC.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo  titulo é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O titulo precisa ter entre 3 ou 60 caracteres")]
         public string Título { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+
         public DateTime DataLancamento { get; set; }
+
         public string Genero { get; set; }
+
+        [Range(1,1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo valor")]
+        [Column(TypeName = "decimal(18,2)")]
         public string Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
         public int Avaliacao { get; set; }
     }
 }
